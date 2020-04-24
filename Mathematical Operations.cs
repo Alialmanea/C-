ae1d45326b9f62ec0d67ac17d1823c614efabe33
ali Rashad almanea <37377385+Alialmanea@users.noreply.github.com>
2020-04-25 01:17:09 +0300

    Update Mathematical Operations.cs

--- Mathematical Operations.cs
+++ Mathematical Operations.cs
@@ -1,38 +1,83 @@
-using System;  
-class Program  
-{  
-static void Main()  
-{  
-//Reading First Number  
-Console.Write("Enter No1: ");  
-int a = Convert.ToInt16(Console.ReadLine());  
-//Reading Second Number  
-Console.Write("Enter No2: ");  
-int b = Convert.ToInt16(Console.ReadLine());  
-Console.WriteLine(" + .Addition");  
-Console.WriteLine(" - .Subtraction");  
-Console.WriteLine(" / .Divsion");  
-Console.WriteLine(" X .Multiplication");    
-//Reading a Choice  
-char c = Convert.ToChar(Console.ReadLine());  
-switch(c)  
-{  
-case +:  
-   Console.WriteLine("Addition Of Two Numbers : "+(a+b));  
-   break;  
-case -:  
-   Console.WriteLine("Subtraction Of Two Numbers : " + (a - b));  
-   break;  
-case /:  
-    Console.WriteLine("Division Of Two Numbers : " + (a / b));  
-    break;  
-case x:  
-   Console.WriteLine("Multiplicaion Of Two Numbers : " + (a * b));  
-   break;    
-default:  
-   Console.WriteLine("Choose Only + - / X ");  
-break;  
-}  
-Console.ReadLine();  
-}  
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        //Reading First Number
+        there:
+        Console.Write("Enter No1: ");
+        int a = Convert.ToInt16(Console.ReadLine());
+        //Reading Second Number  
+        Console.Write("Enter No2: ");
+        int b = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine(" + .Addition");
+        Console.WriteLine(" - .Subtraction");
+        Console.WriteLine(" / .Divsion");
+        Console.WriteLine(" X .Multiplication");
+        //Reading a Choice  
+        char c = Convert.ToChar(Console.ReadLine());
+        string yorN;
+        switch (c)
+        {
+            case '+':
+                Console.WriteLine("Addition Of Two Numbers : " + (a + b));
+                Console.WriteLine("Do you want to make anothor operation ? Y/N");
+                yorN = Console.ReadLine();
+                if (yorN == "Yes" || yorN == "Y")
+                {
+                    goto there;
+                }
+                else
+                    goto Out;
+                break;
+            case '-':
+                Console.WriteLine("Subtraction Of Two Numbers : " + (a - b));
+                Console.WriteLine("Do you want to make anothor operation ? Y/N");
+                yorN = Console.ReadLine();
+                if (yorN == "Yes" || yorN == "Y")
+                {
+                    goto there;
+                }
+                else
+                    goto Out;
+                break;
+            case '/':
+                Console.WriteLine("Division Of Two Numbers : " + (a / b));
+                Console.WriteLine("Do you want to make anothor operation ? Y/N");
+                yorN = Console.ReadLine();
+                if (yorN == "Yes" || yorN == "Y")
+                {
+                    goto there;
+                }
+                else
+                    goto Out;
+                break;
+            case 'x':
+                Console.WriteLine("Multiplicaion Of Two Numbers : " + (a * b));
+                Console.WriteLine("Do you want to make anothor operation ? Y/N");
+                yorN = Console.ReadLine();
+                if (yorN == "Yes" || yorN == "Y")
+                {
+                    goto there;
+                }
+                else
+                    goto Out;
+                break;
+            default:
+                Console.WriteLine("Choose Only + - / X ");
+                Console.WriteLine("Do you want to make anothor operation ? Y/N");
+                 yorN = Console.ReadLine();
+                if (yorN == "Yes" || yorN == "Y")
+                {
+                    goto there;
+                }
+                else
+                    goto Out;
+                break;
+        }
+       Out: 
+        Console.ReadLine();
+    }
 }
